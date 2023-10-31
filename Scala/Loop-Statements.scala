e55100b843534c350e2d96792c429e5873a323b7
Pongsak J <PongsakDev@users.noreply.github.com>
2023-11-01 05:01:48 +0700

    Create Loop-Statements.scala

---
+++ Scala/Loop-Statements.scala
@@ -0,0 +1,12 @@
+//Scala - Loop Statements
+//EX1
+object Demo {
+   def main(args: Array[String]) {
+      var a = 10;
+      
+      // An infinite loop.
+      while( true ){
+         println( "Value of a: " + a );
+      }
+   }
+}
